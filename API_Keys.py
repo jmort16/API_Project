85651cbae644cba55ea0894bd9695756e18373cf
jmort16 <90971714+jmort16@users.noreply.github.com>
2021-11-11 08:32:51 -0600

    Add files via upload

---
+++ API_Keys.py
@@ -0,0 +1,6 @@
+#Federal Bureau of Investigation
+#Crime Data Explorer
+FBI_api_key="HXzuNSaaIO46x38N0iVVKpMDgrP6GG3mxBcacgRH"
+
+#U.S. Bureau of Labor Statistics
+BLS_api_key="489c914f30b34dac8ebc65feb25a15df"
